22fbec06f4b7bb27012a67c9d5ec08789cec9895
Ancor Gonzalez Sosa <ancor@suse.de>
2017-06-12 17:12:53 +0200

    WIP

--- test/boot_requirements_strategies/analyzer_test.rb
+++ test/boot_requirements_strategies/analyzer_test.rb
@@ -23,15 +23,95 @@
 require_relative "../spec_helper"
 require "y2storage"
 
+RSpec.shared_examples "boot disk in devicegraph" do
+  context "if no there is no filesystem mounted at '/' in the devicegraph" do
+    it "returns a Disk object" do
+    end
+
+    it "returns the first disk in the system" do
+    end
+
+    context "if a partition is mounted as '/' in the devicegraph" do
+      it "returns a Disk object" do
+      end
+
+      it "returns the disk containing the '/' partition" do
+      end
+    end
+
+    context "if a LVM LV is mounted as '/' in the devicegraph" do
+      it "returns a Disk object" do
+      end
+
+      it "returns the first disk containing a PV of the involved LVM VG" do
+      end
+    end
+  end
+end
+
 describe Y2Storage::BootRequirementsStrategies::Analyzer do
+  let(:scenario) { "mixed_disks" }
+  let(:devicegraph) { fake_devicegraph }
+  let(:planned_devs) { [] }
+  let(:boot_name) { "" }
+
+  before { fake_scenario(scenario) }
+
   describe ".new" do
+    let(:planned_devs) do
+      [ planned_partition(mount_point: "/"), planned_partition(mount_point: "/boot") ]
+    end
+
+    # There was such a bug, test added to avoid regression"
     it "does not modify the passed collections" do
-      skip "There was such a bug, add test to avoid regression"
+      initial_graph = devicegraph.dup
+      described_class.new(devicegraph, planned_devs, boot_name)
+
+      expect(planned_devs.map(&:mount_point)).to eq ["/", "/boot"]
+      expect(devicegraph.actiongraph(from: initial_graph)).to be_empty
     end
   end
 
   describe "#boot_disk" do
-    pending
+    subject(:analyzer) { described_class.new(devicegraph, planned_devs, boot_name) }
+
+    context "if the name of the boot disk is known and the disk exists" do
+      let(:boot_name) { "/dev/sdb" }
+
+      it "returns a Disk object" do
+        expect(analyzer.boot_disk).to be_a Y2Storage::Disk
+      end
+
+      it "returns the disk matching the given name" do
+        expect(analyzer.boot_disk.name).to eq boot_name
+      end
+    end
+
+    context "if no name is given or there is no such disk" do
+      context "but '/' is in the list of planned devices" do
+        context "and the disk to allocate the planned device is known" do
+          context "and disk exists" do
+            it "returns a Disk object" do
+            end
+
+            it "returns the disk bla" do
+            end
+          end
+
+          context "but the disk does not exist" do
+            include_examples "boot disk in devicegraph"
+          end
+        end
+
+        context "and the disk for '/' is not decided" do
+          include_examples "boot disk in devicegraph"
+        end
+      end
+
+      context "/ not in planned" do
+        include_examples "boot disk in devicegraph"
+      end
+    end
   end
 
   describe "#root_in_lvm?" do
